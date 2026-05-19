a62e6e6efe0dbb4ef0412b420787036ce9269842
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-19 19:16:16 +0200

    Mis cambios locales

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/ProductoService.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/ProductoService.java
@@ -6,16 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.salesianostriana.dam.model.Producto;
+import com.salesianostriana.dam.model.TipoMascota;
 import com.salesianostriana.dam.repository.ProductoRepository;
 import com.salesianostriana.dam.services.base.BaseServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
 public class ProductoService extends BaseServiceImpl<Producto, Long, ProductoRepository>{
 
-	
-	private final ProductoRepository productoRepository;
+	//Producto que estan filtrados por el tipo de mascota
+	public List<Producto> findByTipoMascota(TipoMascota tipo) {
+		return productoRepository.findAll().stream()
+				.filter(p -> p.getTipoMascota() == tipo)
+				.toList();
+	}
 	
 }
