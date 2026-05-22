ed22f141d9fe4c96fb6138b3c4db929503dabaf8
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-22 21:36:48 +0200

    Ya puedo agregar un cliente

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/controller/ClienteController.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/controller/ClienteController.java
@@ -2,11 +2,16 @@ package com.salesianostriana.dam.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.salesianostriana.dam.model.Cliente;
 import com.salesianostriana.dam.services.ClienteService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @Controller
@@ -21,6 +26,22 @@ public class ClienteController {
 	public String listAll (Model model) {
 		model.addAttribute("clientes", clienteService.findAll());
 		return "cliente-list";
+	}
+	
+	@GetMapping("/nuevo")
+	public String showForm(Model model) {
+		model.addAttribute("cliente", new Cliente());
+		return "cliente-form";
+	}
+	
+	@PostMapping("/save")
+	public String save(@Valid @ModelAttribute("cliente") Cliente c, 
+						BindingResult result) {
+		if(result.hasErrors()) {
+			return "cliente-form";
+		}
+		clienteService.save(c);
+		return "redirect:/clientes/";
 	}
 	
 }
