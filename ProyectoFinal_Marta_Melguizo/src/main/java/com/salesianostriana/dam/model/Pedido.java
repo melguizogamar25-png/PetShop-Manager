6ad6ebdd454a7ec07373057f41c1812a9d47855b
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-23 16:58:10 +0200

    Cambios en pedido

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/model/Pedido.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/model/Pedido.java
@@ -11,29 +11,47 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
-@Data @NoArgsConstructor @AllArgsConstructor
+@Data 
+@NoArgsConstructor 
+@AllArgsConstructor
+@Builder
 public class Pedido {
 
 	@Id
 	private Long codigo;
 	
+	@NotNull(message = "La fecha de pedido es obligatoria.")
+	@PastOrPresent(message = "La fecha no puede ser futura.")
 	private LocalDate fecha;
+	
 	private double total;
+	
+	@Size(max = 300, message = "La descripción no supera los 300 caracteres.")
 	private String descripcion;
 	
+	@NotNull(message = "El estado es obligatorio.")
 	@Enumerated(EnumType.STRING)
 	private EstadoPedido estadoPedido;
 	
-	
-	@ManyToOne //Un pedido solo tiene un cliente
+	//Un pedido solo tiene un cliente
+	@ManyToOne 
 	@JoinColumn (name= "cliente_id")
 	private Cliente cliente;
 	
-	@OneToMany //Un pedido tiene unas cuantas de lineas de pedido
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	//Un pedido tiene unas cuantas de lineas de pedido
+	@OneToMany 
 	private List <LineaPedido> lineas = new ArrayList<>(); 
 }
