d1628155093a15002815a72fc2ab215fdc99ee4b
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-25 20:58:55 +0200

    LineaPedidoCOntroller comenzada

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/controller/ListaPedidoController.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/controller/ListaPedidoController.java
@@ -1,5 +1,18 @@
 package com.salesianostriana.dam.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.salesianostriana.dam.services.PedidoService;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/pedidos/{codigo}/lineas")
 public class ListaPedidoController {
+
+	private final PedidoService pedidoService;
+	
 
 }
