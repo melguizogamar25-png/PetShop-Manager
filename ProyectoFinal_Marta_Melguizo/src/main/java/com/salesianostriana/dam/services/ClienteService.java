634f1ac624fc0b5aff549ae76bc442adb42c5105
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-26 20:47:07 +0200

    Logica de negocios de cliente terminada

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/ClienteService.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.salesianostriana.dam.services;
 
+import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,4 +31,32 @@ public class ClienteService extends BaseServiceImpl<Cliente, Long, ClienteReposi
 		return repository.findBySocioTiendaTrue();
 	}
 	
+	//La logica de negocio
+	// - Cuenta cuantos socios hay
+	public long totalSOcios() {
+		return findAll().stream()
+				.filter(Cliente::isSocioTienda)
+				.count();
+	}
+	
+	/* - Gasto total del cliente y suma 
+	tambien el total de todos los pedidos*/
+	public double gastoTotalCliente(long clienteId) {
+		return findById(clienteId)
+				.map(c -> c.getPedidos().stream()
+						.mapToDouble(p -> p.getTotal())
+						.sum())
+				.orElse(0.0);
+				
+	}
+	
+	// - Una lista de los clientes que mas han gastado
+	  public List<Cliente> clientesPorGastoStream() {
+	        return findAll().stream()
+	                .sorted(Comparator.comparingDouble(
+	                        (Cliente c) -> c.getPedidos().stream()
+	                                .mapToDouble(p -> p.getTotal()).sum()
+	                ).reversed())
+	                .toList();
+	}
 }
