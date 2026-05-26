abf1b04141a8ce261c25858ba7e8368a93b5f5af
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-26 23:48:26 +0200

    Consultas derivadas de pedidoService terminadas

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/PedidoService.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/PedidoService.java
@@ -1,7 +1,11 @@
 package com.salesianostriana.dam.services;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.salesianostriana.dam.model.EstadoPedido;
 import com.salesianostriana.dam.model.LineaPedido;
 import com.salesianostriana.dam.model.Pedido;
 import com.salesianostriana.dam.repository.PedidoRepository;
@@ -10,6 +14,46 @@ import com.salesianostriana.dam.services.base.BaseServiceImpl;
 @Service
 public class PedidoService extends BaseServiceImpl<Pedido, Long, PedidoRepository>{
 
+	//COnsultas derivadas
+	
+	public List<Pedido> pedidosPorRango(LocalDate desde, LocalDate hasta) {
+		return repository.findByFechaBetweenOrderByFechaDesc(desde, hasta);
+	}
+	
+	// - Pedidos de un cliente por su id
+	public List<Pedido> pedidosDeCliente(Long clienteId) {
+		return repository.findByClienteIdOrderByFechaDesc(clienteId);
+	}
+	
+	// - Los pedidos filtrados por su estado
+	public List<Pedido> pedidosPorEstado(EstadoPedido estado) {
+		return repository.findByEstadoPedido(estado);
+	}
+	
+	public List<Pedido> top5PorTotal() {
+		return repository.findTop5ByOrderByTotalDesc();
+	}
+	
+	// - EL total facturado de los pedidos que ya estan entregados
+	public Double toralFacturado() {
+		Double result; 
+		result = repository.sumTotalFacturado();
+		return result != null ? result : 0.0;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	//Recalcular el total
 	public void recalcularTotal(Pedido p) {
 		double total = p.getLineas().stream()
