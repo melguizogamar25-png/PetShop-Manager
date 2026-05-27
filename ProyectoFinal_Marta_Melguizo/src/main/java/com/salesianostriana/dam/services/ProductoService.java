4dad3e3f311b28531825bac2bf593c77a50e4099
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-27 16:36:12 +0200

    Metodos terminados

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/ProductoService.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/services/ProductoService.java
@@ -1,7 +1,10 @@
 package com.salesianostriana.dam.services;
 
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
@@ -50,5 +53,16 @@ public class ProductoService extends BaseServiceImpl<Producto, Long, ProductoRep
 				.mapToDouble(Producto::getPrecio)
 				.average() //Calcula la media
 				.orElse(0.0);
+	}
+	
+	public Map<TipoMascota, List<Producto>> agrupadosPorTipo() {
+		return findAll().stream()
+				.collect(Collectors.groupingBy(Producto::getTipoMascota));
+	}
+	
+	public List<Producto> ordenadosPorPrecioAsc() {
+		return findAll().stream()
+				.sorted(Comparator.comparingDouble(Producto::getPrecio))
+				.toList();
 	}
 }
