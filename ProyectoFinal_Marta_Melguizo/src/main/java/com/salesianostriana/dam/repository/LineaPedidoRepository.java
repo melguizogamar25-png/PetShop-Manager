8b5d9d952c81692844903fbd3c204853133ff27a
Marta Melguizo García <melguizo.gamar25@triana.salesianos.edu>
2026-05-28 10:31:38 +0200

    Terminado

--- ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/repository/LineaPedidoRepository.java
+++ ProyectoFinal_Marta_Melguizo/src/main/java/com/salesianostriana/dam/repository/LineaPedidoRepository.java
@@ -1,10 +1,15 @@
 package com.salesianostriana.dam.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.salesianostriana.dam.model.LineaPedido;
+import com.salesianostriana.dam.model.Pedido;
 
 public interface LineaPedidoRepository extends JpaRepository <LineaPedido, Long>{
 
-	
+	//Consultas
+	// - Buscar un pedido
+	List<LineaPedido> findByPedido(Pedido pedido);
 }
